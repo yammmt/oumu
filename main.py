e47df934c1c5cb7e395a22b330a01612f69624b4
ya@rasp <cu.cs.y20@gmail.com>
2016-06-06 20:57:26 +0900

    issue #1 and some rename

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ import speak
 print("Please talk to me!")
 
 #listen.julius_begin()
-words_talked_to = listen.listen_to_julius()
-print(words_talked_to)
+listen.connect_to_julius()
 
-speak.speak_with_jsay(words_talked_to)
+while(True):
+    words_talked_to = listen.listen_to_julius()
+    print(words_talked_to)
+    speak.speak_with_jsay(words_talked_to)
+    if "さよなら" in words_talked_to or "さようなら" in words_talked_to:
+        exit()
+
